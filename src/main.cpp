3cfbcc0dacf5078c2aa8e9fe5de0d977877fb736
Abidan Brito <43681148+abidanBrito@users.noreply.github.com>
2025-12-27 08:40:26 +0000

    feat: exit builtin command

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include <string>
 
+void print_prompt();
+bool handle_input(const std::string &input);
+void handle_invalid_input(const std::string &input);
+void repl_loop();
+
+int main() {
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  repl_loop();
+
+  return 0;
+}
+
 void print_prompt() { std::cout << "$ "; }
+
+bool handle_input(const std::string &input) {
+  if (input == "exit") {
+    return false;
+  }
+
+  handle_invalid_input(input);
+  return true;
+}
 
 void handle_invalid_input(const std::string &input) {
   std::cout << input << ": command not found" << std::endl;
@@ -11,21 +34,13 @@ void repl_loop() {
   while (true) {
     print_prompt();
 
-    std::string command;
-    if (!std::getline(std::cin, command)) {
+    std::string input;
+    if (!std::getline(std::cin, input)) {
       break;
     }
 
-    handle_invalid_input(command);
+    if (!handle_input(input)) {
+      break;
+    }
   }
-}
-
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  repl_loop();
-
-  return 0;
 }
