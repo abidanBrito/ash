1634a1c964a58f26c244a7dfbf090651482b70f3
Abidan Brito <43681148+abidanBrito@users.noreply.github.com>
2025-12-27 08:19:46 +0000

    feat: repl loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <string>
 
+void print_prompt() { std::cout << "$ "; }
+
+void handle_invalid_input(const std::string &input) {
+  std::cout << input << ": command not found" << std::endl;
+}
+
+void repl_loop() {
+  while (true) {
+    print_prompt();
+
+    std::string command;
+    if (!std::getline(std::cin, command)) {
+      break;
+    }
+
+    handle_invalid_input(command);
+  }
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+  repl_loop();
 
-  std::string command;
-  std::getline(std::cin, command);
-  std::cout << command << ": command not found" << std::endl;
+  return 0;
 }
